6b850d977e33564c193b4656a04ac87aee95105a
temportalflux <temportalflux@users.noreply.github.com>
2022-12-16 17:01:27 +0000

    deploy: 41f846807db3041f94960fd75a4aa40993f77730

--- wishlist-36db4978b2788b73.js
+++ wishlist-36db4978b2788b73.js
@@ -638,7 +638,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('wishlist-bf51aead67ee508c_bg.wasm', import.meta.url);
+        input = new URL('wishlist-36db4978b2788b73_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
