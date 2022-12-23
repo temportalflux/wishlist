16b1e1b553e5669fef3c59fd0565286439692327
temportalflux <temportalflux@users.noreply.github.com>
2022-12-23 17:32:29 +0000

    deploy: 23c13b56edc43ca44cec1a3d0389bff4cb66af96

--- wishlist-4132d667622d7398.js
+++ wishlist-4132d667622d7398.js
@@ -789,15 +789,15 @@ function getImports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1560 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1556 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 806, __wbg_adapter_28);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1715 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1711 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 826, __wbg_adapter_31);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1879 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1875 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 868, __wbg_adapter_34);
         return addHeapObject(ret);
     };
@@ -837,7 +837,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('wishlist-70b2abff07c45d66_bg.wasm', import.meta.url);
+        input = new URL('wishlist-4132d667622d7398_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
