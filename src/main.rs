5c43db8896e5654702d78250e7e71114386cff13
temportalflux <dustin.yost.t@gmail.com>
2023-10-20 17:42:34 -0400

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use yew::prelude::*;
 
+mod logging;
+
 #[cfg(target_family = "wasm")]
 fn main() {
-	logging::wasm::init(logging::wasm::Config::default().prefer_target());
+	logging::init(logging::Config::default().prefer_target());
 	yew::Renderer::<App>::new().render();
 }
 
